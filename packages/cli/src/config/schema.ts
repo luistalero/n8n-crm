90250a88aaa8e75209a94f5a0b3e79172d696937
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-08-22 14:29:54 -0500

    n8n funcion

---
+++ packages/cli/src/config/schema.ts
@@ -0,0 +1,75 @@
+import { GlobalConfig } from '@n8n/config';
+import { Container } from '@n8n/di';
+
+export const schema = {
+	executions: {
+		mode: {
+			doc: 'If it should run executions directly or via queue',
+			format: ['regular', 'queue'] as const,
+			default: 'regular',
+			env: 'EXECUTIONS_MODE',
+		},
+
+		// A Workflow times out and gets canceled after this time (seconds).
+		// If the workflow is executed in the main process a soft timeout
+		// is executed (takes effect after the current node finishes).
+		// If a workflow is running in its own process is a soft timeout
+		// tried first, before killing the process after waiting for an
+		// additional fifth of the given timeout duration.
+		//
+		// To deactivate timeout set it to -1
+		//
+		// Timeout is currently not activated by default which will change
+		// in a future version.
+		timeout: {
+			doc: 'Max run time (seconds) before stopping the workflow execution',
+			format: Number,
+			default: -1,
+			env: 'EXECUTIONS_TIMEOUT',
+		},
+		maxTimeout: {
+			doc: 'Max execution time (seconds) that can be set for a workflow individually',
+			format: Number,
+			default: 3600,
+			env: 'EXECUTIONS_TIMEOUT_MAX',
+		},
+	},
+
+	userManagement: {
+		/**
+		 * @important Do not remove until after cloud hooks are updated to stop using convict config.
+		 */
+		isInstanceOwnerSetUp: {
+			// n8n loads this setting from DB on startup
+			doc: "Whether the instance owner's account has been set up",
+			format: Boolean,
+			default: false,
+		},
+
+		authenticationMethod: {
+			doc: 'How to authenticate users (e.g. "email", "ldap", "saml")',
+			format: ['email', 'ldap', 'saml'] as const,
+			default: 'email',
+		},
+	},
+
+	/**
+	 * @important Do not remove until after cloud hooks are updated to stop using convict config.
+	 */
+	endpoints: {
+		rest: {
+			format: String,
+			default: Container.get(GlobalConfig).endpoints.rest,
+		},
+	},
+
+	/**
+	 * @important Do not remove until after cloud hooks are updated to stop using convict config.
+	 */
+	ai: {
+		enabled: {
+			format: Boolean,
+			default: Container.get(GlobalConfig).ai.enabled,
+		},
+	},
+};
